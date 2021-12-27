64bca664895fb2e32b1ee15712f7ff49137ed816
Hristijan <hristijan2509@gmail.com>
2021-12-28 00:49:05 +0100

    initial commit after creating a repository

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+//			$(function () {
+//				$("#clickToScroll a").on('click', function (e) {
+//					e.preventDefault();
+//					var navigationHref = $("#aboutMe");
+//					$('html, body').animate({ scrollTop: $(navigationHref).offset().top }, 1500,linear function () {
+//						window.location.navigationHref = navigationHref;
+//                        
+//                  
+//					});
+//				});        
+                
+//                        $("#clickToScroll a").click(function (){
+//                $('html, body').animate({
+//                    scrollTop: $("#aboutMe").offset().top
+//                }, 2000);
+//            });
+//    $("#clickToScroll a").click(function (){
+//    $('html, body').animate({
+//				scrollTop: parseInt($("#aboutMe").offset().top)
+//			}, 2000);    
+//			return false;
+//             });             
+                
+});
